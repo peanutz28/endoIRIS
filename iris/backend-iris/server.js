d3469ad275c3d5bbd47df5e09db128396b166b73
Srao2020 <YOUR-NOREPLY@users.noreply.github.com>
2025-11-14 16:00:50 -0500

    serverscreenerupdate

--- iris/backend-iris/server.js
+++ iris/backend-iris/server.js
@@ -1,22 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import chatRouter from "./chat/chat.js";
 import prepRouter from "./prep/prep.js";
+import screenerRouter from "../../screener/screenerRoute.js";  
 
 dotenv.config();
 
 const app = express();
-
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use("/chat", chatRouter);
 app.use("/prep", prepRouter);
+app.use("/screener", screenerRouter);
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`IRIS backend running on http://localhost:${PORT}`);
